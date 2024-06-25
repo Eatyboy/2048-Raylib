b6ad04ea9eb2d98f4ec0b4ef656f3e201f4e8e19
Ethan Carter <eatyboy32@gmail.com>
2024-06-25 16:23:42 -0700

    added tile spawn animation

--- main.c
+++ main.c
@@ -10,7 +10,7 @@
 #define BWIDTH 4
 #define BHEIGHT 4
 
-#define ANIMDT (0.1f)
+#define ANIMDT (0.01f)
 
 typedef struct Anim {
 	int dx;
@@ -45,7 +45,7 @@ void restartGame(Tile board[BHEIGHT][BWIDTH],
 				int *collisionCount,
 				bool *spawningTiles);
 void printTiles(Tile tiles[BHEIGHT][BWIDTH]);
-void generateTile(Tile tiles[BHEIGHT][BWIDTH]);
+void generateTile(Tile tiles[BHEIGHT][BWIDTH], int *animCount);
 bool isFullBoard(Tile board[BHEIGHT][BWIDTH]);
 void endAnims(Tile tiles[BHEIGHT][BWIDTH],
 				Collision collisions[BHEIGHT * BWIDTH],
@@ -81,8 +81,8 @@ int main(void) {
 	}
 	int activeCollisions = 0;
 
-	generateTile(tiles);
-	generateTile(tiles);
+	generateTile(tiles, &activeAnims);
+	generateTile(tiles, &activeAnims);
 
 	while (!WindowShouldClose()) {
 		if (isFullBoard(tiles)) gameOver();
@@ -120,13 +120,24 @@ int main(void) {
 
 						int delta = 0;
 						bool willCombine = false;
+						int nextNum = 0;
 
 						for (int k = i-1; k >= 0; k--) {
 							int collidedValue = tiles[k][j].num;
 
 							if (collidedValue == 0) {
 								delta++;
-							} else if (collidedValue == value) {
+								continue;
+							} 
+
+							if (nextNum == 0) {
+								nextNum = collidedValue;
+							} else if (nextNum == collidedValue) {
+								delta++;
+								nextNum = -1;
+							}
+
+							if (collidedValue == value) {
 								delta++;
 								willCombine = true;
 							}
@@ -159,13 +170,24 @@ int main(void) {
 
 						int delta = 0;
 						bool willCombine = false;
+						int nextNum = 0;
 
 						for (int k = i+1; k <= 3; ++k) {
 							int collidedValue = tiles[k][j].num;
 
 							if (collidedValue == 0) {
 								delta++;
-							} else if (collidedValue == value) {
+								continue;
+							} 
+
+							if (nextNum == 0) {
+								nextNum = collidedValue;
+							} else if (nextNum == collidedValue) {
+								delta++;
+								nextNum = -1;
+							}
+
+							if (collidedValue == value) {
 								delta++;
 								willCombine = true;
 							}
@@ -198,13 +220,24 @@ int main(void) {
 
 						int delta = 0;
 						bool willCombine = false;
+						int nextNum = 0;
 
 						for (int k = j-1; k >= 0; k--) {
 							int collidedValue = tiles[i][k].num;
 
 							if (collidedValue == 0) {
 								delta++;
-							} else if (collidedValue == value) {
+								continue;
+							} 
+
+							if (nextNum == 0 ) {
+								nextNum = collidedValue;
+							} else if (nextNum == collidedValue) {
+								delta++;
+								nextNum = -1;
+							}
+
+							if (collidedValue == value) {
 								delta++;
 								willCombine = true;
 							}
@@ -237,13 +270,24 @@ int main(void) {
 
 						int delta = 0;
 						bool willCombine = false;
+						int nextNum = 0;
 
 						for (int k = j+1; k <= 3; ++k) {
 							int collidedValue = tiles[i][k].num;
 
 							if (collidedValue == 0) {
 								delta++;
-							} else if (collidedValue == value) {
+								continue;
+							} 
+
+							if (nextNum == 0) {
+								nextNum = collidedValue;
+							} else if (nextNum == collidedValue) {
+								delta++;
+								nextNum = -1;
+							}
+
+							if (collidedValue == value) {
 								delta++;
 								willCombine = true;
 							}
@@ -271,7 +315,7 @@ int main(void) {
 		updateAnims(tiles, collisions, &activeAnims, &activeCollisions);
 
 		if (shouldSpawnTile && activeAnims == 0) {
-			generateTile(tiles);
+			generateTile(tiles, &activeAnims);
 			shouldSpawnTile = false;
 		}
 
@@ -310,20 +354,32 @@ int main(void) {
 					float tileY = (tile.anim != NULL)
 						? Lerp(i, i + tile.anim->dy, 1 - tile.anim->t)
 						: i;
+					float size = 1.0f;
+					if (tile.anim != NULL) {
+						if (tile.anim->dx == 0 && tile.anim->dy == 0) {
+							size -= tile.anim->t;
+						}
+					}
 
 					DrawRectangleRounded(
 						(Rectangle){
-							boardPos.x + thick + tileX * (innerDim + thick),
-							boardPos.y + thick + tileY * (innerDim + thick),
-							innerDim, 
-							innerDim
+							boardPos.x + thick
+								+ tileX * (innerDim + thick)
+								+ (1.0f - size) * 0.5f * innerDim,
+							boardPos.y + thick
+								+ tileY * (innerDim + thick)
+								+ (1.0f - size) * 0.5f * innerDim,
+							innerDim * size, 
+							innerDim * size
 						},
 						0.05f, 0, numColors[(int)log2(tile.num) - 1]
 					);
-					DrawText(TextFormat("%d", tile.num), 
-						boardPos.x + 50 + tileX * (innerDim + thick),
-						boardPos.y + 30 + tileY * (innerDim + thick),
-						80, BLACK);
+					if (FloatEquals(size, 1.0f)) {
+						DrawText(TextFormat("%d", tile.num), 
+							boardPos.x + 50 + tileX * (innerDim + thick),
+							boardPos.y + 30 + tileY * (innerDim + thick),
+							80, BLACK);
+					}
 				}
 			}
 			//Draw board
@@ -332,17 +388,23 @@ int main(void) {
 	return 0;
 }
 
-void generateTile(Tile tiles[BHEIGHT][BWIDTH]) {
+void generateTile(Tile tiles[BHEIGHT][BWIDTH], int *animCount) {
 	int possibleNums[] = {2,4};
 	int newIndex;
 	do {
 		newIndex = GetRandomValue(0, 15);
 	} while (tiles[newIndex % BHEIGHT][newIndex / BWIDTH].num != 0);
+
 	int newNum = possibleNums[GetRandomValue(0,1)];
+	Anim *newAnim = malloc(sizeof(Anim));
+	newAnim->dx = 0;
+	newAnim->dy = 0;
+	newAnim->t = 1;
 	tiles[newIndex % BHEIGHT][newIndex / BWIDTH] = (Tile){
 		newNum,
-		NULL
+		newAnim
 	};
+	(*animCount)++;
 }
 
 void printTiles(Tile tiles[BHEIGHT][BWIDTH]) {
@@ -408,8 +470,8 @@ void restartGame(Tile board[BHEIGHT][BWIDTH],
 	collisionCount = 0;
 	spawningTiles = false;
 
-	generateTile(board);
-	generateTile(board);
+	generateTile(board, animCount);
+	generateTile(board, animCount);
 }
 
 void updateAnims(Tile tiles[BHEIGHT][BWIDTH],
@@ -425,44 +487,51 @@ void updateAnims(Tile tiles[BHEIGHT][BWIDTH],
 			if (!FloatEquals(anim->t, 0.0f)) {
 				anim->t = fmaxf(anim->t - ANIMDT, 0.0f);
 			} else {
-				int newX = j + anim->dx;
-				int newY = i + anim->dy;
-				bool isCollision = false;
-
-				for (int k = 0; k < *collisionCount; ++k) {
-					Collision collision = collisions[k];
-					if (collision.x == newX && collision.y == newY) {
-						isCollision = true;
-						for (int l = k; l < *collisionCount - 1; ++l) {
-							collisions[l] = collisions[l+1];
-						}
-						(*collisionCount)--;
-						collisions[*collisionCount] = NULL_COLLISION;
-						break;
-					}
-				}
-
-				if (isCollision) {
-					#if debug
-					printf("Collisions: ");
-					#endif
-					for (size_t i = 0; i < *collisionCount; i++) {
-						Collision c = collisions[i];
-						printf("(%d, %d)", c.x, c.y);
-					}
-					#if debug
-					printf("\ncollision at %d, %d\n", newX, newY);
-					#endif
-					tiles[newY][newX].num *= 2;
-					tiles[i][j].num = 0;
+				if (anim->dx == 0 && anim->dy == 0) {
 					tiles[i][j].anim = NULL;
+					#if debug
+					printf("Tile at (%d, %d) spawned\n", j, i);
+					#endif
 				} else {
-					tiles[i][j].anim = NULL;
-					tiles[newY][newX] = tiles[i][j];
-					tiles[i][j].num = 0;
-					#if debug
-					printf("Tile at (%d, %d) moved to (%d, %d)\n", j, i, newX, newY);
-					#endif
+					int newX = j + anim->dx;
+					int newY = i + anim->dy;
+					bool isCollision = false;
+
+					for (int k = 0; k < *collisionCount; ++k) {
+						Collision collision = collisions[k];
+						if (collision.x == newX && collision.y == newY) {
+							isCollision = true;
+							for (int l = k; l < *collisionCount - 1; ++l) {
+								collisions[l] = collisions[l+1];
+							}
+							(*collisionCount)--;
+							collisions[*collisionCount] = NULL_COLLISION;
+							break;
+						}
+					}
+
+					if (isCollision) {
+						#if debug
+						printf("Collisions: ");
+						#endif
+						for (size_t i = 0; i < *collisionCount; i++) {
+							Collision c = collisions[i];
+							printf("(%d, %d)", c.x, c.y);
+						}
+						#if debug
+						printf("\ncollision at %d, %d\n", newX, newY);
+						#endif
+						tiles[newY][newX].num *= 2;
+						tiles[i][j].num = 0;
+						tiles[i][j].anim = NULL;
+					} else {
+						tiles[i][j].anim = NULL;
+						tiles[newY][newX] = tiles[i][j];
+						tiles[i][j].num = 0;
+						#if debug
+						printf("Tile at (%d, %d) moved to (%d, %d)\n", j, i, newX, newY);
+						#endif
+					}
 				}
 
 				free(anim);
